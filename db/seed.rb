fc3fda88939b0f9cb07066a1caeba2f64645c49d
Nelson <nelsonwittwer@gmail.com>
2012-08-15 22:21:23 -0600

    added everything, but gets/puts is not working for whatever reason

--- db/seed.rb
+++ db/seed.rb
@@ -1,7 +1,15 @@
 # Cleaning Out
 Network.delete_all
 Show.delete_all
+Sport.delete_all
 amc = Network.create(name: "AMC")
 nbc = Network.create(name: "NBC")
 Show.create(name: "Mad Men", day_of_week: "Sunday", hour_of_day: 22, network: amc)
 Show.create(name: "Community", day_of_week: "Thursday", hour_of_day: 20, network: nbc)
+Show.create(name: "Voltron", day_of_week: "Monday", hour_of_day: 23, network: nbc)
+Show.create(name: "Who's The Boss?", day_of_week: "Wednesday", hour_of_day: 13, network: nbc)
+Sport.create(name:"Wakeboarding")
+Sport.create(name:"Snowboarding")
+Sport.create(name: "Skiing")
+Sport.create(name: "Soccer")
+Sport.create(name: "Football")
--- watchman.rb
+++ watchman.rb
@@ -5,11 +5,27 @@ require "./db/setup"
 Dir.glob('./models/*').each { |r| require r}
 require "./db/seed"
 
-puts "There are #{Show.count} in the database"
 
-Network.all.each do |network|
-	puts "Shows airing on #{network}"
-	network.shows.each do |show|
-		puts show
-	end	
-end
+# puts "What day of the week are you looking for shows to watch?"
+# input_day = gets
+
+# Show.where(day_of_week: input_day).each do |s|
+# 	puts s
+# end
+
+puts Sport.all
+puts Show.all
+# Sport.all
+# puts "what sport would you like me to regurgitate to you today?"
+# s = gets
+
+# puts Sport.where("name = #{s.to_s}")
+
+
+
+# Network.all.where(day_of_week: input_day).each do |network|
+# 	puts "Shows airing on #{network}"
+# 	network.shows.each do |show|
+# 		puts show
+# 	end	
+# end
